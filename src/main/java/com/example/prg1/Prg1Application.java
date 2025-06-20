f3389f098759900301f39e5a096d2d7ea19d6177
bluetree2 <kmsid77@gmail.com>
2025-06-20 09:32:47 +0900

    first

---
+++ src/main/java/com/example/prg1/Prg1Application.java
@@ -0,0 +1,13 @@
+package com.example.prg1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prg1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prg1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prg1/Prg1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prg1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prg1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
